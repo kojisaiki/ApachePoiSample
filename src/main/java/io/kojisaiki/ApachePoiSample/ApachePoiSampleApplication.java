b81f585b3c6b4bb5a4ecb64f944c98f8a4e77c1a
saiki <saiki@dcr.co.jp>
2017-10-01 14:32:48 +0900

    generated by Spring Initializr

---
+++ src/main/java/io/kojisaiki/ApachePoiSample/ApachePoiSampleApplication.java
@@ -0,0 +1,12 @@
+package io.kojisaiki.ApachePoiSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApachePoiSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApachePoiSampleApplication.class, args);
+	}
+}
---
+++ src/test/java/io/kojisaiki/ApachePoiSample/ApachePoiSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package io.kojisaiki.ApachePoiSample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApachePoiSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
