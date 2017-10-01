6761b59c77247d1e85adf61aa22abbabc7cbca24
saiki <saiki@dcr.co.jp>
2017-10-01 14:49:28 +0900

    AP側の枠組み作成（サンプルの本質には関係なし）

---
+++ src/main/java/io/kojisaiki/ApachePoiSample/service/ExcelService.java
@@ -0,0 +1,13 @@
+package io.kojisaiki.ApachePoiSample.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExcelService {
+
+    public String generateSimpleExcel() {
+        String ret = "filepath?";
+
+        return ret;
+    }
+}
---
+++ src/main/java/io/kojisaiki/ApachePoiSample/web/rest/ExcelResource.java
@@ -0,0 +1,28 @@
+package io.kojisaiki.ApachePoiSample.web.rest;
+
+import io.kojisaiki.ApachePoiSample.service.ExcelService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/excel")
+public class ExcelResource {
+
+    private ExcelService excelService;
+
+    public ExcelResource(
+            ExcelService excelService
+    ) {
+        this.excelService = excelService;
+    }
+
+    @GetMapping("/simple")
+    public String getSimpleExcel() {
+        String ret;
+
+        ret = this.excelService.generateSimpleExcel();
+
+        return ret;
+    }
+}
