4fe129d8c0a29fb24fdf08d718b31969132d9782
saiki <saiki@dcr.co.jp>
2017-10-01 15:40:57 +0900

    生成したxlsxファイルをダウンロードさせる

--- src/main/java/io/kojisaiki/ApachePoiSample/web/rest/ExcelResource.java
+++ src/main/java/io/kojisaiki/ApachePoiSample/web/rest/ExcelResource.java
@@ -1,9 +1,18 @@
 package io.kojisaiki.ApachePoiSample.web.rest;
 
 import io.kojisaiki.ApachePoiSample.service.ExcelService;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/api/excel")
@@ -18,11 +27,18 @@ public class ExcelResource {
     }
 
     @GetMapping("/simple")
-    public String getSimpleExcel() throws Exception {
-        String ret;
+    public ResponseEntity<Resource> getSimpleExcel() throws Exception {
+        String generatedFilePath = this.excelService.generateSimpleExcel();
 
-        ret = this.excelService.generateSimpleExcel();
+        File generatedFile = new File(generatedFilePath);
+        Path absolutePath = Paths.get(generatedFile.getAbsolutePath());
 
-        return ret;
+        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(absolutePath));
+
+        return ResponseEntity.ok()
+                .header("Content-disposition", "attachment;filename=" + generatedFile.getName())
+                .contentType(MediaType.parseMediaType("application/vnd.ms-excel"))
+                .contentLength(generatedFile.length())
+                .body(resource);
     }
 }
